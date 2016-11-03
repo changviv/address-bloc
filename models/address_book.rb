abb8b012dd3d4510c09c6579ce14ae0465f005ac
Vivien Chang <vchang103@gmail.com>
2016-11-03 00:25:47 -0700

    Checkpoint 3 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,10 +10,10 @@ class AddressBook
   def add_entry(name,phone_number,email)
     index = 0
     entries.each do |entry|
-      if name <entry.name
+      if name < entry.name
         break
       end
-      index+=1
+      index += 1
     end
     entries.insert(index, Entry.new(name,phone_number,email))
   end
